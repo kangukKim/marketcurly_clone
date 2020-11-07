de56a47b259ef9fd53123804375329b0027782a3
root <root@ip-172-31-47-33.ap-northeast-2.compute.internal>
2020-11-07 10:55:52 +0000

    ignore테스트2

--- pdos/DatabasePdo.php
+++ pdos/DatabasePdo.php
@@ -5,7 +5,7 @@ function pdoSqlConnect()
 {
     try {
         $DB_HOST = "database-2.caaxfeb6vg5f.ap-northeast-2.rds.amazonaws.com";
-        $DB_NAME = "marketcurly";
+        $DB_NAME = "marketcurly_tmp";
         $DB_USER = "admin";
         $DB_PW = "kimku0540)";
         $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
